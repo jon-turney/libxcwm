5a9040767fa39b0d9a701cac5002b728980dcc4d
Jess VanDerwalker <washu@sonic.net>
2012-07-19 11:12:53 -0700

    libxcwm: Comment and printf changes in event_loop.c
    Changed the comments and removed/commented out printfs for
    XCB_CONFIGURE_NOTIFY and XCB_MOTION_NOTIFY cases in run_event_loop().
    
    Signed-off-by: Jess VanDerwalker <washu@sonic.net>
    
    Reviewed-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- src/libxcwm/event_loop.c
+++ src/libxcwm/event_loop.c
@@ -231,7 +231,6 @@ run_event_loop(void *thread_arg_struct)
             {
                 /* We don't actually allow our client to create its
                  * window here, wait until the XCB_MAP_REQUEST */
-                printf("Got create notify\n");
                 break;
             }
 
@@ -328,11 +327,10 @@ run_event_loop(void *thread_arg_struct)
 
             case XCB_MOTION_NOTIFY:
             {
-                //printf("X mouse motion from from xserver-");
-                xcb_button_press_event_t *bp =
-                    (xcb_button_press_event_t *)evt;
-                // printf ("mouse motion in window %ld, at coordinates (%d,%d)\n",
-                //        bp->event, bp->event_x, bp->event_y );
+                /* xcb_button_press_event_t *bp = */
+                /*     (xcb_button_press_event_t *)evt; */
+                /* printf ("mouse motion in window %u, at coordinates (%d,%d)\n", */
+                /*        bp->event, bp->event_x, bp->event_y ); */
                 break;
             }
 
